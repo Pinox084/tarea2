0bb6e93d2df8e2e8594751ffa87d753fc655642c
igbarria1314 <114494885+igbarria1314@users.noreply.github.com>
2022-10-25 23:01:29 -0300

    clase moneda heredable

--- codigo/src/codigo/Moneda.java
+++ codigo/src/codigo/Moneda.java
@@ -9,30 +9,3 @@ public abstract class Moneda {
     }
     
 }
-class Moneda1000 extends Moneda{
-    public Moneda1000(){
-        super();
-    }
-    @Override
-    public int getValor(){
-        return super.getValor()+1000;
-    }
-}
-class Moneda500 extends Moneda{
-    public Moneda500(){
-        super();
-    }
-    @Override
-    public int getValor(){
-        return super.getValor()+500;
-    }
-    
-}
-class Moneda100 extends Moneda{
-    public Moneda100(){
-        super();
-    }
-    public int getValor(){
-        return super.getValor()+100;
-    }
-}
